ed94bac97ce8e7c6e757c3606717473444098b5b
Mohamed-Mitwaly <iMeto@Mohameds-MacBook-Pro.local>
2014-07-20 15:57:07 +0200

    Initial Commit

---
+++ MMGDragAndDrop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MMGDragAndDrop
+//
+//  Created by Mohamed Mitwaly on 7/20/14.
+//  Copyright (c) 2014 MMG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
